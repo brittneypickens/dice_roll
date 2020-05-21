fa8174113cab9bffd914b51dbe3dd556e4e9bfc9
brittney pickens <brittneypickens@brittneys-mbp.home>
2020-05-21 15:59:19 -0400

    added

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ const dieRolls = []
 const numberOfDice = document.querySelector('#number-of-dice')
 const total =document.querySelector('total')
 
-    rollButton.addEventListener("click", function() {
+    roll.addEventListener("click", function() {
         let input = roll.value
         let index = 0
 
         while (index < input) {
             let total = Math.floor(Math.random(0) * 6) + 1
             dieRolls.push(total)
+            numberOfDice += total
+            index += 1
         }
+           sum.innerHTML + numberOfDice
+           console.log(dieRolls)
+
             
     })
